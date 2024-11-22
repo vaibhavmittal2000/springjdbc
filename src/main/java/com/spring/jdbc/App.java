eb94346a4fca668360807d308e45357d667f79c9
Vaibhav <vaibhavmittal2017@gmail.com>
2024-11-22 15:04:44 +0530

    database setup and insert operation implementation

--- src/main/java/com/spring/jdbc/App.java
+++ src/main/java/com/spring/jdbc/App.java
@@ -1,13 +1,24 @@
 package com.spring.jdbc;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import com.spring.jdbc.dao.StudentDao;
+import com.spring.jdbc.entities.Student;
+
+public class App {
+    public static void main( String[] args ){
+        System.out.println( "Program started..." );
+        ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
+        StudentDao studentDao = context.getBean("studentDao",StudentDao.class);
+        
+        Student student = new Student();
+        student.setId(302);
+        student.setName("Vivek");
+        student.setCity("Rohtak");
+        
+        int result = studentDao.insert(student);
+        System.out.println("Student added: "+result);
     }
 }
