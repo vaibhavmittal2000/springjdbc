6e970c7b1f105f7b8b30f8fd641c5e92ccbd29a5
Vaibhav <vaibhavmittal2017@gmail.com>
2024-11-22 18:38:16 +0530

    delete and selecting data

--- src/main/java/com/spring/jdbc/App.java
+++ src/main/java/com/spring/jdbc/App.java
@@ -1,9 +1,9 @@
 package com.spring.jdbc;
 
+import java.util.List;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
-
 import com.spring.jdbc.dao.StudentDao;
 import com.spring.jdbc.entities.Student;
 
@@ -13,20 +13,35 @@ public class App {
         ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
         StudentDao studentDao = context.getBean("studentDao",StudentDao.class);
         
+//        Insert Operation
 //        Student student = new Student();
 //        student.setId(302);
 //        student.setName("Vivek");
 //        student.setCity("Rohtak");
-//        
 //        int result = studentDao.insert(student);
 //        System.out.println("Student added: "+result);
         
-        Student student = new Student();
-        student.setId(123);
-        student.setName("Karan");
-        student.setCity("Dadri");
+//        Update Operation        
+//        Student student = new Student();
+//        student.setId(123);
+//        student.setName("Karan");
+//        student.setCity("Dadri");
+//        int result = studentDao.change(student);
+//        System.out.println("Data changed: "+result);
         
-        int result = studentDao.change(student);
-        System.out.println("Data changed: "+result);
+//        Delete Operation
+//        int result = studentDao.delete(300);
+//        System.out.println("Data deleted: "+result);
+        
+//        Select Single Data
+//        Student student = studentDao.getStudent(222);
+//        System.out.println(student);
+        
+//        Select Multiple Data
+        List<Student> students = studentDao.getAllStudents();
+        for(Student s:students) {
+        	System.out.println(s);
+        }
+        
     }
 }
--- src/main/java/com/spring/jdbc/dao/StudentDaoImpl.java
+++ src/main/java/com/spring/jdbc/dao/StudentDaoImpl.java
@@ -1,6 +1,9 @@
 package com.spring.jdbc.dao;
 
+import java.util.List;
+
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import com.spring.jdbc.entities.Student;
 
 public class StudentDaoImpl implements StudentDao{
@@ -20,6 +23,31 @@ public class StudentDaoImpl implements StudentDao{
 		String query = "update student set name=? , city=? where id=?";
         int r = this.jdbcTemplate.update(query,student.getName(),student.getCity(),student.getId());
 		return r;
+	}
+	
+	//Deletion
+	public int delete(int studentId) {
+		//Delete Query
+		String query = "delete from student where id=?";
+        int r = this.jdbcTemplate.update(query,studentId);
+		return r;
+	}
+	
+	//Selecting Single Object
+	public Student getStudent(int studentId) {
+		//Selecting single student data
+		String query = "select * from student where id=?";
+		RowMapper<Student> rowMapper = new RowMapperImpl();
+		Student student = this.jdbcTemplate.queryForObject(query, rowMapper,studentId);
+		return student;
+	}
+	
+	//Selecting Multiple Objects
+	public List<Student> getAllStudents() {
+		//Selecting multiple students data
+		String query = "select * from student";
+		List<Student> students = this.jdbcTemplate.query(query, new RowMapperImpl());
+		return students;
 	}
 	
 	public JdbcTemplate getJdbcTemplate() {
