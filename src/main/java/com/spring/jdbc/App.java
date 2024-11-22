df08c438ba3ef3d328f9d102ee384e3b18e0bc26
Vaibhav <vaibhavmittal2017@gmail.com>
2024-11-22 19:35:42 +0530

    spring jdbc without XML configuration using java file config

--- src/main/java/com/spring/jdbc/App.java
+++ src/main/java/com/spring/jdbc/App.java
@@ -2,6 +2,7 @@ package com.spring.jdbc;
 
 import java.util.List;
 import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 import org.springframework.jdbc.core.JdbcTemplate;
 import com.spring.jdbc.dao.StudentDao;
@@ -10,7 +11,8 @@ import com.spring.jdbc.entities.Student;
 public class App {
     public static void main( String[] args ){
         System.out.println( "Program started..." );
-        ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
+        //ApplicationContext context = new ClassPathXmlApplicationContext("com/spring/jdbc/config.xml");
+        ApplicationContext context = new AnnotationConfigApplicationContext(JdbcConfig.class);
         StudentDao studentDao = context.getBean("studentDao",StudentDao.class);
         
 //        Insert Operation
